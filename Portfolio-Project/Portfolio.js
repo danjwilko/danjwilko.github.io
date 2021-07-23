8bcc8e7cb7134be85f35d0e5fa9a3d0c69fd734a
Daniel Wilkinson <68111031+danjwilko@users.noreply.github.com>
2021-07-23 13:47:48 +0100

    commit

--- Portfolio-Project/Portfolio.js
+++ Portfolio-Project/Portfolio.js
@@ -1,7 +1,7 @@
 window.onscroll = function() {myFunction()};
 
 function myFunction() {
-  if (document.body.scrollTop > 1000|| document.documentElement.scrollTop > 2000) {
+  if (document.body.scrollTop > 1000|| document.documentElement.scrollTop > 1800) {
     document.getElementById("emphasis").className = "on-scroll";
   } else {
     document.getElementById("emphasis").className = "";
