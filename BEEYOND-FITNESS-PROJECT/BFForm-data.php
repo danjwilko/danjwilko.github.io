2a0c1ecff2ce96dfaf49b50cddb9c199a78a07af
Daniel Wilkinson <68111031+danjwilko@users.noreply.github.com>
2021-10-31 09:08:58 +0000

    Updates
    Page tweaks, form updates, php added.

--- BEEYOND-FITNESS-PROJECT/BFForm-data.php
+++
@@ -1,9 +0,0 @@
-<?php
-    mail('yazbeeyondfitness@gmail.com', $_POST['name'], $_POST['phone'], $_POST['email'], $_POST['message']);
-?>
-<p>Your email has been sent.</p>
-
-<?php
-    mail('youremail@example.com', $_POST['subject'], $_POST['message']);
-?>
-<p>Your email has been sent.</p>
